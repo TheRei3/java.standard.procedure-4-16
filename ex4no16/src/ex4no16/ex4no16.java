6d7583505b079c85bffa771d71f2e7a3095be8e0
admin <admin@DESKTOP-8L2IG92>
2021-05-25 07:07:41 +0900

    ex4no16

---
+++ ex4no16/src/ex4no16/ex4no16.java
@@ -0,0 +1,14 @@
+package ex4no16;
+
+public class ex4no16 { // 162p
+	public static void main(String[] args) {
+		
+		for( int i = 1 ; i <= 5 ; i++) {
+			for( int j = 1 ; j <= 10 ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
